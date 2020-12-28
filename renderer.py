7668f47d338647cef8685ff9472b197a4e7afd73
Christa Bridges <christa@pebblepad.co.uk>
2020-12-28 12:31:04 +0000

    operator-buttons Added operator buttons and resized all so they are equal.

--- renderer.py
+++ renderer.py
@@ -15,22 +15,40 @@ class Renderer():
         # outputBox.grid(row=0, column=0)
 
         # Create number buttons
-        button0 = Button(root, text="0", bg="#2D2D2D", fg="#EEEEEE", padx=5, pady=5)
-        button0.config(command=partial(self.calculator.append_expression, button0.cget("text")))
-        button0.grid(row=4, column=1)
+        button_0 = Button(root, text="0", bg="#2D2D2D", fg="#EEEEEE", width=3, pady=5)
+        button_0.config(command=partial(self.calculator.append_expression, button_0.cget("text")))
+        button_0.grid(row=4, column=0)
 
         currentRow = 3
         currentCol = 0
 
         for i in range (1, 10):
-            currentButton = Button(root, text=str(i), bg="#2D2D2D", fg="#EEEEEE", padx=5, pady=5)
-            currentButton.config(command=partial(self.calculator.append_expression, currentButton.cget("text")))
-            currentButton.grid(row=currentRow, column=currentCol)
+            current_button = Button(root, text=str(i), bg="#2D2D2D", fg="#EEEEEE", width=3, pady=5)
+            current_button.config(command=partial(self.calculator.append_expression, current_button.cget("text")))
+            current_button.grid(row=currentRow, column=currentCol)
 
             currentCol += 1
-            if int(currentButton.cget("text")) % 3 == 0:
+            if int(current_button.cget("text")) % 3 == 0:
                 currentRow -= 1
                 currentCol = 0
+
+        # Operator Buttons
+        operators = ["+", "−", "×", "÷"]
+        currentRow = 1
+
+        for operator in operators:
+            current_button = Button(root, text=operator, bg="#2D2D2D", fg="#EEEEEE", width=3, pady=5)
+            current_button.config(command=partial(self.calculator.append_expression, current_button.cget("text")))
+            current_button.grid(row=currentRow, column=4)
+            currentRow += 1
+
+        decimal_button = Button(root, text=".", bg="#2D2D2D", fg="#EEEEEE", width=3, pady=5)
+        decimal_button.config(command=partial(self.calculator.append_expression, decimal_button.cget("text")))
+        decimal_button.grid(row=4, column=1)
+
+        equals_button = Button(root, text="=", bg="#2D2D2D", fg="#EEEEEE", width=3, pady=5)
+        equals_button.config(command=partial(self.calculator.append_expression, equals_button.cget("text")))
+        equals_button.grid(row=4, column=2)
 
         root.mainloop()
 
