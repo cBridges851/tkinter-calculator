09adfa65e7dc1da371f4ad1ef790a8bc181a8a7d
Christa Bridges <christa@pebblepad.co.uk>
2020-12-29 08:43:09 +0000

    operator-buttons Added documentation to calculator.py.

--- calculator.py
+++ calculator.py
@@ -1,7 +1,16 @@
 class Calculator():
+    '''
+        The class that manages the input of the expression and processes it to create a final answer.
+    '''
     def __init__(self):
+        '''
+            Initialises the calculator by setting a starting value to the expression.
+        '''
         self.expression = ""
 
     def append_expression(self, value):
+        '''
+            Appends values to the expression.
+        '''
         self.expression += value
         print(self.expression)
